505a1cddd31dfc1936677988853d1a2e42e9a60b
Alan Michel <alan.pereira@skyone.solutions>
2022-02-17 09:31:51 -0300

    Edited path static and hosts

--- okrs/settings.py
+++ okrs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$u2-4ae!he7@_0fqzg)ckj1rd0m!ps^5pdw$@5(tv@m$015qbz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
