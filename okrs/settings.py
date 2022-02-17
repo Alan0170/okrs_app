c1da537845f19fa6c763721cb325a82c05c29f4f
Alan Michel <alan.pereira@skyone.solutions>
2022-02-16 23:49:19 -0300

    Added static directory

--- okrs/settings.py
+++ okrs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$u2-4ae!he7@_0fqzg)ckj1rd0m!ps^5pdw$@5(tv@m$015qbz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
